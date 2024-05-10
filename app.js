7682720cf3ef47269864c28cb2406743342235a0
osama shaker <152613379+OsamaShaker0@users.noreply.github.com>
2024-05-10 07:05:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 // rest of packages
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
@@ -19,7 +24,18 @@ const authRouter = require('./routes/authRoutes');
 //middlewares
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-const { process_params } = require('express/lib/router');
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
